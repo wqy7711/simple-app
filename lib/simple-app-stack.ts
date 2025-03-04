9d524742de4d54b64ab7857e0c1358c8a8c5d2cb
unknown <944325009@qq.com>
2025-03-04 03:00:51 +0000

    Lambda communicating with DynamoDB

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -36,6 +36,33 @@ export class SimpleAppStack extends cdk.Stack {
       tableName: "Movies",
     });
 
+    const getMovieByIdFn = new lambdanode.NodejsFunction(
+      this,
+      "GetMovieByIdFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambdas/getMovieById.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: moviesTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+    );
+
+    const getMovieByIdURL = getMovieByIdFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
+
+    moviesTable.grantReadData(getMovieByIdFn)
+
+    new cdk.CfnOutput(this, "Get Movie Function Url", { value: getMovieByIdURL.url });
+
     new custom.AwsCustomResource(this, "moviesddbInitData", {
       onCreate: {
         service: "DynamoDB",
